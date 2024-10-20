db509fa1dffb9ecfbd9ed4b378f2ea0ffc8f451e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-20 15:49:08 -0700

    Update Replace-with-the-alphabet-position.js

--- Replace-with-the-alphabet-position.js
+++ Replace-with-the-alphabet-position.js
@@ -25,3 +25,14 @@ function alphabetPosition(text) {
 }
 
 // or
+
+function alphabetPosition(text) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.toLowerCase().split('')
+  .filter( letter => {
+    let index = alphabet.indexOf(letter);
+    return index > -1;
+  }  )
+  .map( letter => alphabet.indexOf(letter) + 1 )
+  .join(' ')
+}
