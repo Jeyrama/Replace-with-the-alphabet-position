1441cd6c5eb2a320f4c4b14a2c5c003b40d70b01
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-20 15:48:32 -0700

    Update Replace-with-the-alphabet-position.js

--- Replace-with-the-alphabet-position.js
+++ Replace-with-the-alphabet-position.js
@@ -13,3 +13,13 @@ Example:
 
 
 // Solution
+
+function alphabetPosition(text) {
+  let result = "";
+  for (let i = 0; i < text.length; i++){
+    let code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length-1);
+}
